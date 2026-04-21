6886a05b3c3ac7dffa67ae6aeb92fac8d4ed132a
Jan de Leeuw <jan@deleeuwpdx.net>
2026-04-21 10:42:27 -0400

    simple

--- triple.R
+++ triple.R
@@ -1,24 +1,88 @@
-# example 1
+
+library(RSpectra)
 
 set.seed(12345)
-rmat <- crossprod(matrix(rnorm(400), 100, 4))/100
-cmat <- crossprod(matrix(rnorm(300), 100, 3))/100
-a <- rnorm(4)^2
-b <- rnorm(3)^2
-c <- rnorm(4)^2
-d <- rnorm(3)^2
-m <- 3
-z <- as.vector(m + outer(a, b, "+") + outer(c, d))
-ymat <- matrix(rpois(12, z), 4, 3)
-wmat <- 1 / sqrt(xmat)
-aini <- 1:4
-bini <- 1:3
-cini <- 1:4
-dini <- 1:3
-mini <- 1
-ztilde <- mini + outer(aini, bini, "+") + outer(cini, dini)
-xtilde <- wmat * (ymat - ztilde)
-gtilde <- rmat %*% xtilde %*% cmat
-vtilde <- xtilde - vtilde
-krx <- matrix(kr %*% x, 4, 3)
-l <- sum(x * (kr %*% x))
+ymat <- matrix(rnorm(12), 4, 3)
+yold <- matrix(rnorm(12), 4, 3)
+rmat <- crossprod(matrix(rnorm(40), 10, 4)) / 10
+cmat <- crossprod(matrix(rnorm(30), 10, 3)) / 10
+wmat <- matrix(rnorm(12)^2, 4, 3)
+
+rckr <- kronecker(cmat, rmat)
+yvec <- as.vector(ymat)
+wvec <- as.vector(wmat)
+wdig <- diag(wvec)
+
+print(matrix(rckr %*% yvec, 4, 3))
+print(rmat %*% ymat %*% cmat)
+
+print(matrix((rckr %*% (wvec * yvec)) / wvec, 4, 3))
+print((rmat %*% (wmat * ymat) %*% cmat) / wmat)
+
+checker <- function(x, y, rmat, cmat, wmat) {
+  sx <- loss(x, rmat, cmat, wmat)
+  so <- loss(y, rmat, cmat, wmat)
+  lb <- eigs_sym(rmat, 1)$values * eigs_sym(cmat, 1)$values
+  go <- rmat %*% y %*% cmat
+  vo <- y - go/lb
+  s1 <- so + 2 * sum(go * (x - y)) + lb * sum((x - y)^2)
+  s2 <- so + lb * sum((x - vo)^2) - sum(go^2)/lb
+  print(c(sx, s1, s2))
+  print(c(loss(x - y, rmat, cmat), lb * sum((x - y)^2)))
+}
+
+loss <- function(x, rmat, cmat) {
+  return(sum((rmat %*% x %*% cmat) * x))
+}
+
+triple <- function(ymat,
+                   wmat = array(1, dim(ymat)),
+                   rmat = diag(nrow(ymat)),
+                   cmat = diag(ncol(ymat)),
+                   p = 2,
+                   zini = NULL,
+                   itmax = 100,
+                   inmax = 1,
+                   eps = 1e-6,
+                   verbose = TRUE) {
+  nr <- nrow(ymat)
+  nc <- ncol(ymat)
+  labd <- eigs_sym(rmat, 1)$values * eigs_sym(cmat, 1)$values
+  if (is.null(zini)) {
+    zold <- eckart_young(ymat, p)
+  } else {
+    zold <- zinit
+  }
+  rold <- wmat * (ymat - zold)
+  sold <- loss(rold, rmat, cmat)
+  itel <- 1
+  repeat {
+    gold <- rmat %*% zold %*% cmat
+    ynew <- zold - (gold / wmat) / labd
+    znew <- eckart_young(ynew, p)
+    rnew <- wmat * (ymat - znew)
+    snew <- loss(rnew, rmat, cmat)
+    epsi <- max(abs(zold - znew))
+    if (verbose) {
+      cat("itel", formatC(itel, digits = 3, format = "d"),
+          "sold", formatC(sold, digits = 10, width = 15, format = "f"),
+          "snew", formatC(snew, digits = 10, width = 15, format = "f"),
+          "epsi", formatC(epsi, digits = 10, width = 15, format = "f"),
+          "\n")
+    }
+    if ((itel == itmax) || (epsi < eps)) {
+      break
+    }
+    itel <- itel + 1
+    zold <- znew
+    sold <- snew
+  }
+}
+
+eckart_young <- function(x, p) {
+  h <- svd(x, nu = p, nv = p)
+  return(tcrossprod(h$u, h$v %*% diag(h$d[1:p])))
+}
+
+
+
